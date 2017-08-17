4bf984fa92b2a819afae54bc16f3c8eb6abaefb8
2017zhang <649924507@qq.com>
2017-08-17 22:11:56 +0800

    Update backstage.js

--- backstage.js
+++ backstage.js
@@ -4,12 +4,12 @@ angular.module('routingDemoApp',['ngRoute'])
             templateUrl: '0.html',
             // controller: 'loginCtrl'
         })
-    .when('/1', {
-            templateUrl: '1.html',
-            // controller: '1Ctrl'
-        })
+            .when('/1', {
+                templateUrl: '1.html',
+                // controller: '1Ctrl'
+            })
             .when('/2', {
-            templateUrl: '2.html',
-            // controller: '2Ctrl'
-        }).otherwise({redirectTo: '/login'});
+                templateUrl: '2.html',
+                // controller: '2Ctrl'
+            }).otherwise({redirectTo: '/login'});
     }]);
